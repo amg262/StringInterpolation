9b98ec2dc1bf8c10934b8b04e004d8478b4d43b2
Andrew <andrewmgunn26@gmail.com>
2021-09-12 18:50:52 -0500

    damn near finished

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 
-namespace StringInterpolation   
+namespace StringInterpolation
 {
     class Program
     {
@@ -18,18 +18,18 @@ namespace StringInterpolation
             {
                 // create data file
 
-                 // ask a question
+                // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
                 int weeks = int.Parse(Console.ReadLine());
 
-                 // determine start and end date
+                // determine start and end date
                 DateTime today = DateTime.Now;
                 // we want full weeks sunday - saturday
-                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                DateTime dataEndDate = today.AddDays(-(int) today.DayOfWeek);
                 // subtract # of weeks from endDate to get startDate
                 DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-                
+
                 // random number generator
                 Random rnd = new Random();
 
@@ -45,18 +45,86 @@ namespace StringInterpolation
                         // generate random number of hours slept between 4-12 (inclusive)
                         hours[i] = rnd.Next(4, 13);
                     }
+
                     // M/d/yyyy,#|#|#|#|#|#|#
                     //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
                     sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
                     // add 1 week to date
                     dataDate = dataDate.AddDays(7);
                 }
+
                 sw.Close();
             }
             else if (resp == "2")
             {
                 // TODO: parse data file
+                string file = "data.txt";
 
+                /*
+                 * Display error immediately if file not found
+                 */
+                if (!File.Exists(file))
+                {
+                    Console.WriteLine($"Error: {file} does not exist");
+                }
+                else
+                {
+                    /*
+                     * Create streamreader object and loop thru file before EOF
+                     */
+                    StreamReader reader = new StreamReader(file);
+                    while (!reader.EndOfStream)
+                    {
+                        /*
+                         * Running total vars - string line for readline of file
+                         */
+                        double totalHours = 0;
+                        double average = 0;
+                        double avgFormat = 0;
+                        string input = reader.ReadLine();
+
+                        /*
+                         * splitting string on comma delimiter into array
+                         */
+                        string[] wk = input.Split(',');
+
+                        /*
+                         * create new datetime object - TryParse for validation and conversion
+                         */
+                        DateTime date;
+                        DateTime.TryParse(wk[0], out date);
+
+                        /*
+                         * Handling field delimiting by | containing hours sleep data
+                         */
+                        int[] hr = Array.ConvertAll(wk[1].Split('|'), Int32.Parse);
+
+                        /*
+                         * For each sleep hour value add to running total for avg, total
+                         */
+                        foreach (var value in hr)
+                        {
+                            totalHours += value;
+                        }
+
+                        /*
+                         * Divide hrs by 7 - format to 2 decimal place for output isnt crazy
+                         */
+                        average = totalHours / 7;
+                        avgFormat = Math.Round(average, 2);
+
+                        Console.WriteLine($"Week of {date:MMM}, {date:dd}, {date:yyyy}");
+
+                        Console.WriteLine(
+                            $"{"Sun",4}{"Mon",4}{"Tue",4}{"Wed",4}{"Thu",4}{"Fri",4}{"Sat",4}{"Total",6}{"Average",8}");
+                        Console.WriteLine(
+                            $"{"---",4}{"---",4}{"---",4}{"---",4}{"---",4}{"---",4}{"---",4}{"-----",6}{"-------",8}");
+                        // display hours of sleep for each day
+                        Console.WriteLine(
+                            $"{hr[0],4}{hr[1],4}{hr[2],4}{hr[3],4}{hr[4],4}{hr[5],4}{hr[6],4}{totalHours,6}{avgFormat,8}");
+                        Console.WriteLine();
+                    }
+                }
             }
         }
     }
