be6d085ca420d820411b35f5a2f29b542030b575
Andrew <andrewmgunn26@gmail.com>
2021-09-13 09:21:51 -0500

    added logic for TryParse of If Resp == 1 and invalid integer added for # weeks

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using NLog;
 using NLog.Web;
+
 namespace StringInterpolation
 {
     class Program
@@ -12,10 +13,11 @@ namespace StringInterpolation
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
             Console.WriteLine("Enter anything else to quit.");
-            
+
             string path = Directory.GetCurrentDirectory() + "\\nlog.config";
             var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
-            
+            logger.Info("Program started");
+
             // input response
             string resp = Console.ReadLine();
 
@@ -26,39 +28,51 @@ namespace StringInterpolation
                 // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
-                int weeks = int.Parse(Console.ReadLine());
 
-                // determine start and end date
-                DateTime today = DateTime.Now;
-                // we want full weeks sunday - saturday
-                DateTime dataEndDate = today.AddDays(-(int) today.DayOfWeek);
-                // subtract # of weeks from endDate to get startDate
-                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
 
-                // random number generator
-                Random rnd = new Random();
+                //int weeks = int.Parse(Console.ReadLine());
+                int weeks = 0;
+                Int32.TryParse(Console.ReadLine(), out weeks);
+                string ans = Console.ReadLine();
 
-                // create file
-                StreamWriter sw = new StreamWriter("data.txt");
-                // loop for the desired # of weeks
-                while (dataDate < dataEndDate)
+                if (!int.TryParse(ans, out weeks))
                 {
-                    // 7 days in a week
-                    int[] hours = new int[7];
-                    for (int i = 0; i < hours.Length; i++)
+                    logger.Error("Invalid input (integer): {Answer}", ans);
+                }
+                else
+                {
+                    // determine start and end date
+                    DateTime today = DateTime.Now;
+                    // we want full weeks sunday - saturday
+                    DateTime dataEndDate = today.AddDays(-(int) today.DayOfWeek);
+                    // subtract # of weeks from endDate to get startDate
+                    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+
+                    // random number generator
+                    Random rnd = new Random();
+
+                    // create file
+                    StreamWriter sw = new StreamWriter("data.txt");
+                    // loop for the desired # of weeks
+                    while (dataDate < dataEndDate)
                     {
-                        // generate random number of hours slept between 4-12 (inclusive)
-                        hours[i] = rnd.Next(4, 13);
+                        // 7 days in a week
+                        int[] hours = new int[7];
+                        for (int i = 0; i < hours.Length; i++)
+                        {
+                            // generate random number of hours slept between 4-12 (inclusive)
+                            hours[i] = rnd.Next(4, 13);
+                        }
+
+                        // M/d/yyyy,#|#|#|#|#|#|#
+                        //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+                        // add 1 week to date
+                        dataDate = dataDate.AddDays(7);
                     }
 
-                    // M/d/yyyy,#|#|#|#|#|#|#
-                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
-                    // add 1 week to date
-                    dataDate = dataDate.AddDays(7);
+                    sw.Close();
                 }
-
-                sw.Close();
             }
             else if (resp == "2")
             {
@@ -144,6 +158,7 @@ namespace StringInterpolation
                     }
                 }
             }
+
             logger.Info("Program ended");
         }
     }
